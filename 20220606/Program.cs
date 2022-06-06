8d075dc947a22cf08a790fc2bf958490ba30a9df
Johanna Kirchner <johanna.kirchner@programmers.com.br>
2022-06-06 10:38:36 -0300

    20220606

---
+++ 20220606/Program.cs
@@ -0,0 +1,19 @@
+﻿
+int nota = int.Parse(Console.ReadLine());
+
+if (nota >= 8)
+{
+    Console.WriteLine("aprovado");
+}
+else if (nota < 8 && nota >= 5)
+{
+    Console.WriteLine("recuperação");
+}
+else
+{
+    Console.WriteLine("reprovado");
+}
+
+var resultado = (nota >= 8 ? "aprovado" : ((nota < 8 && nota >= 5) ? "recuperação" : "reprovado"));
+
+Console.WriteLine(resultado);
